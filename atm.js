8edda29f51093e9c173408175f51fd41a11735e7
Jermaine Smikle <jermaine.smikle@aol.com>
2018-07-03 15:06:24 -0400

    Learn Javascript loops to dynamically create images

--- atm.js
+++ atm.js
@@ -1,1 +1,21 @@
-alert("JavaScript is working!");
+
+var tvShows = ['bb_theory.jpeg', 'fresh_prince.jpeg', 'martin.jpeg', 'scrubs.jpg', 'simpsons.jpeg']
+
+var gallery = document.getElementById('gallery');
+var count = 0;
+
+/* for(thisItem in thisSet){
+ do this
+}
+*/
+
+for (thisItem in tvShows) {
+    //print img tag
+    //set img src ="images/" + saiyans[count]
+    var newImage = document.createElement('img');
+    newImage.src = 'images/' + tvShows[count];
+    newImage.addClass = 'responsive';
+    newImage.addClass = 'image-container';
+    gallery.appendChild(newImage);
+    count = count + 1;
+}
